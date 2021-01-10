efd2c92161a202941653e1eb2e90c2cfec7b5dc3
veusch <veusch>
2021-01-10 14:59:26 +0100

    add timer functionality and display in game is working

---
+++ Assets/mygame/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private float timeInSec;
+    public TextMeshProUGUI timer;
+
+    void Start()
+    {
+        timeInSec = 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeInSec -= Time.deltaTime;
+        float minutes = Mathf.FloorToInt(timeInSec / 60);
+        float seconds = Mathf.FloorToInt(timeInSec % 60);
+
+        if (timeInSec > 0)
+        {
+
+            timer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+        else
+        {
+            Debug.Log("stop game");
+        }
+    }
+}
+    
