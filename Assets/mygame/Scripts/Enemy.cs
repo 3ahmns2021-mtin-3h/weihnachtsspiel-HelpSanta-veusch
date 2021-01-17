f1b0c3fe663d18cb4777cd227cb34d801b1df8f5
veusch <veusch>
2021-01-17 14:34:25 +0100

    rigidbody2D and boxcollider2D added and console interaction is working

---
+++ Assets/mygame/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Enemy");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger Hallo Enemy1" + collision.name); 
+    }
+    // Update is called once per frame
+    void Update()   
+    {
+        
+    }
+}
---
+++ Assets/mygame/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    int countCollision = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name=="Enemy")
+        {
+            countCollision--;
+        }
+
+        else if (collision.name == "Enemy2")
+        {
+            countCollision--;
+        }
+
+        else if (collision.name == "Geschenk")
+        {
+            countCollision++;
+        }
+
+
+        Debug.Log("Trigger Hallo Player collide with" + collision.name);
+    }
+}
